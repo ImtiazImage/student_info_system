cdc3ebff2df3a1d723b671f9f0d773a47d73b5f0
S.M.Imtiaz <smimtiaz.cse@gmail.com>
2017-10-28 22:55:13 +0600

    last commit

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php include 'inc/header.php'; ?>
+<?php
+if (!isset($_GET['search']) || $_GET['search'] == NULL) {
+	echo "Please enter keywords";
+} else {
+	$std_name = trim(mysqli_real_escape_string($_GET['name']));
+	$std_email= trim(mysqli_real_escape_string(strtolower($_GET['email'])));
+
+	$query = mysqli_connect($db,)
+}
+
+?>
 
 <div class="container" style="margin-top: 5%;">
     <div class="col-md-6 col-md-offset-3">
 
         <!-- Search Form -->
-        <form role="form-group">
+        <form role="form-group" action="" method="GET">
         
         <!-- Search Field -->
             <div class="row">
@@ -17,25 +28,15 @@
 
 				<!-- form group [search] -->
 			    <div class="form-group">
-			        <label class="filter-col" style="margin-right:0;" >name:</label>
+			        <label class="filter-col" style="margin-right:0;" >Name:</label>
 			        <input type="text" name="name" class="form-control">
 			    </div>
 
 			    <div class="form-group">
-			        <label class="filter-col" style="margin-right:0;">batch:</label>
-			        <input type="text" name="batch" class="form-control">
+			        <label class="filter-col" style="margin-right:0;">E-mail:</label>
+			        <input type="text" name="email" class="form-control">
 			    </div><!-- form group [search] -->
 
-				<div class="form-group">
-			        <label class="filter-col" style="margin-right:0;">Depertment:</label>
-			        <select name="dept" class="form-control">
-			            <option value="cse">CSE</option>
-			            <option value="eee">EEE</option>
-			            <option value="civil">CIVIL</option>
-			            <option value="architecture">ARCHITECTURE</option>
-			            <option value="5">LAW</option>
-			        </select>                                
-			    </div> <!-- form group [rows] -->
 
 			    <div class="form-group">    
 			        <button type="submit" name="search"class="btn btn-default filter-col">
